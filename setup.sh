329cb4730450b75a172aae753006825123cba7e2
nicvac-teach <nicola.vacca@marconibari.edu.it>
2025-10-07 15:51:35 +0000

    Setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+################
+# Setup 
+# Nome del progetto da sincronizzare, già esistente su Apps script
+prjDir = "Archivio_cdc"
+# Id del progetto: Apps script / {progetto} / Impostazioni progetto / ID script / Copia
+prjID = "1poOQ88rBYcHSmdQ8AB87coKtE3GxInRoPZ_sJf_AySuGktr8E8FTeKwP"
+################
+
 if ! which clasp &> /dev/null; then
     sudo npm install -g @google/clasp
 fi
 
+#Login dell'utente Apps script che ha il progetto da sincronizzare
 clasp login
+
+
+# Creo la dir di progetto e la sincronizza con Apps script in remoto
+mkdir -p "${prjDir}"
+
+clasp clone "${prjID}" --rootDir "${prjDir}"
+
+
 
